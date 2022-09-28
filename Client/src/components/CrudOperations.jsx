c4aa0fb7da6b92c891dd5c68d1b838ec35f670e8
AlexEkeh <alex.ekeh@gmail.com>
2022-09-28 16:19:41 +0100

    Application Done

--- Client/src/components/CrudOperations.jsx
+++ Client/src/components/CrudOperations.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const url = 'http://localhost:5000/users';
-const url = 'https://react-mui5-crud-app.herokuapp.com/users';
+const url = "https://react-mui5-crud-app.herokuapp.com/users/"
 
 const addUser = async (data) => {
     try {
--- server.js
+++ server.js
@@ -8,7 +8,12 @@ dotenv.config();
 const port = process.env.PORT;
 
 server.use(router);
-server.use(cors({ origin: true, credentials: true }));
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+server.use(cors(corsOptions));
 server.use(jsonServer.bodyParser);
 
 // Serve static assets if in Production
